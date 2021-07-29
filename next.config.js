72c28071530d38cdf4e3fafdce66708ba13eafab
Babatunde Adenowo <mo.xodeeq@gmail.com>
2021-07-29 19:05:37 +0100

    need stripe business

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
     domains: ["links.papareact.com", "fakestoreapi.com"],
   },
   env: {
-    paystack_public_key: process.env.PAYSTACK_PUBLIC_KEY,
+    stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
   },
 };
